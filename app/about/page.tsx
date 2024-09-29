a257521ce8bbe97a9ccab9eb42cc5ff936e79f03
Kasun Hettiarachchi <kasunh1411@gmail.com>
2024-09-29 12:00:45 +0530

    Some little changes

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'About',
+    description: 'About page',
+  }
+  
+  export default function About() {
+    return (
+      <section>
+        <h1 className="font-semibold text-2xl mb-8 tracking-tighter">About</h1>
+      </section>
+    )
+  }
--- app/page.tsx
+++ app/page.tsx
@@ -6,26 +6,29 @@ export default function Page() {
     <section>
       <div className="flex gap-x-4 items-center flex-row mb-10">
         <Image
-          src={"/image.jpg"}
+          src={"/pp.jpg"}
           alt="profile pic"
           width={100}
           height={100}
           className="rounded-full border-[2px] border-yellow-500 -ml-2 hover:grayscale transition-all duration-300"
         />
         <div>
-          <p className="text-xl font-semibold">John Doe</p>
-          <p className="text-lg text-gray-400">Student at SLIIT</p>
+          <p className="text-xl font-semibold">Kasun Hettiarachchi</p>
+          <p className="text-lg text-gray-400">
+            Software Engineering Undergraduate
+          </p>
         </div>
       </div>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Welcome! 👋
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter ">
+        Welcome to my blog! 👋
       </h1>
       <p className="mb-4">
-        I am a passionate JavaScript developer and full-time student at SLIIT.
-        With a year of self-taught coding experience, I enjoy building web
-        applications and writing about web development as a part-time blogger.
-        I'm a fan of open-source contributions and JAMstack technologies, always
-        seeking new opportunities to collaborate and grow as a developer.
+        An ambitious Software Engineering undergraduate with a passion for
+        Software Engineering, Data Engineering, and Data Science. Committed to
+        developing skills and applying knowledge to real-world projects. Driven
+        to create innovative solutions and contribute to high-impact
+        initiatives. Eager to leverage cutting-edge technologies to develop
+        software that exceeds industry standards.
       </p>
       <div className="my-8">
         <p className="text-gray-400 text-lg mb-2">From my blog</p>
